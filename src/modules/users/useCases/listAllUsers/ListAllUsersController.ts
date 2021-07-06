9f9185d3c7be0301fd85d3b062f091d0b83b334d
Nilo <nilobdrumond@gmail.com>
2021-07-06 11:53:56 -0300

    all tests ok

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,19 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase';
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    const id = Array.isArray(user_id) ? user_id[0] : user_id;
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id: id });
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
